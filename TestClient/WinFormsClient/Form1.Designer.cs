0035f10600ddc89b3425317ea5577032b609f9bf
Edelsheim <skyline7651@gmail.com>
2022-04-19 00:19:53 +0900

    update simple C# TCP Client

--- TestClient/WinFormsClient/Form1.Designer.cs
+++ TestClient/WinFormsClient/Form1.Designer.cs
@@ -28,20 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.ip_textbox = new System.Windows.Forms.TextBox();
+            this.port_textbox = new System.Windows.Forms.TextBox();
+            this.connect_button = new System.Windows.Forms.Button();
+            this.input_textbox = new System.Windows.Forms.TextBox();
+            this.send_button = new System.Windows.Forms.Button();
+            this.close_button = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // ip_textbox
+            // 
+            this.ip_textbox.Location = new System.Drawing.Point(12, 12);
+            this.ip_textbox.Name = "ip_textbox";
+            this.ip_textbox.Size = new System.Drawing.Size(100, 21);
+            this.ip_textbox.TabIndex = 0;
+            this.ip_textbox.Text = "127.0.0.1";
+            // 
+            // port_textbox
+            // 
+            this.port_textbox.Location = new System.Drawing.Point(12, 39);
+            this.port_textbox.Name = "port_textbox";
+            this.port_textbox.Size = new System.Drawing.Size(100, 21);
+            this.port_textbox.TabIndex = 1;
+            this.port_textbox.Text = "3501";
+            // 
+            // connect_button
+            // 
+            this.connect_button.Location = new System.Drawing.Point(135, 12);
+            this.connect_button.Name = "connect_button";
+            this.connect_button.Size = new System.Drawing.Size(75, 23);
+            this.connect_button.TabIndex = 2;
+            this.connect_button.Text = "Connect";
+            this.connect_button.UseVisualStyleBackColor = true;
+            this.connect_button.Click += new System.EventHandler(this.connect_button_Click);
+            // 
+            // input_textbox
+            // 
+            this.input_textbox.Location = new System.Drawing.Point(13, 84);
+            this.input_textbox.Name = "input_textbox";
+            this.input_textbox.Size = new System.Drawing.Size(197, 21);
+            this.input_textbox.TabIndex = 3;
+            this.input_textbox.Text = "hello";
+            // 
+            // send_button
+            // 
+            this.send_button.Location = new System.Drawing.Point(216, 82);
+            this.send_button.Name = "send_button";
+            this.send_button.Size = new System.Drawing.Size(75, 23);
+            this.send_button.TabIndex = 4;
+            this.send_button.Text = "Send";
+            this.send_button.UseVisualStyleBackColor = true;
+            this.send_button.Click += new System.EventHandler(this.send_button_Click);
+            // 
+            // close_button
+            // 
+            this.close_button.Location = new System.Drawing.Point(135, 41);
+            this.close_button.Name = "close_button";
+            this.close_button.Size = new System.Drawing.Size(75, 23);
+            this.close_button.TabIndex = 5;
+            this.close_button.Text = "Close";
+            this.close_button.UseVisualStyleBackColor = true;
+            this.close_button.Click += new System.EventHandler(this.close_button_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(974, 501);
+            this.Controls.Add(this.close_button);
+            this.Controls.Add(this.send_button);
+            this.Controls.Add(this.input_textbox);
+            this.Controls.Add(this.connect_button);
+            this.Controls.Add(this.port_textbox);
+            this.Controls.Add(this.ip_textbox);
             this.Name = "Form1";
             this.Text = "Main";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox ip_textbox;
+        private System.Windows.Forms.TextBox port_textbox;
+        private System.Windows.Forms.Button connect_button;
+        private System.Windows.Forms.TextBox input_textbox;
+        private System.Windows.Forms.Button send_button;
+        private System.Windows.Forms.Button close_button;
     }
 }
 
--- TestClient/WinFormsClient/Form1.cs
+++ TestClient/WinFormsClient/Form1.cs
@@ -1,20 +1,56 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.Sockets;
 
 namespace WinFormsClient
 {
     public partial class Form1 : Form
     {
+        private TcpClient client = null;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void connect_button_Click(object sender, EventArgs e)
+        {
+            if (this.client != null)
+            {
+                this.client.Close();
+                this.client?.Dispose();
+                this.client = null;
+            }
+
+            if (this.client == null)
+            {
+                this.client = new TcpClient(ip_textbox.Text, int.Parse(port_textbox.Text));
+            }
+        }
+
+        private void send_button_Click(object sender, EventArgs e)
+        {
+            if (this.client != null)
+            {
+                if (this.client.Connected)
+                {
+                    using (NetworkStream networkStream = this.client.GetStream())
+                    {
+                        string send = this.input_textbox.Text;
+                        byte[] send_bytes = Encoding.Default.GetBytes(send);
+                        networkStream.Write(send_bytes, 0, send_bytes.Length);
+                    }
+                }
+            }
+        }
+
+        private void close_button_Click(object sender, EventArgs e)
+        {
+            if (this.client != null) {
+                this.client.Close();
+                this.client?.Dispose();
+            }
+            this.client = null;
         }
     }
 }
